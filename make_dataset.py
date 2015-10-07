a4a87061446853d6130e9df09c7354d320b1bb97
John Arevalo <johnarevalo@gmail.com>
2015-10-07 14:02:23 -0500

    Remove unused definition

--- make_dataset.py
+++ make_dataset.py
@@ -22,7 +22,6 @@ chars = list(set(data))
 vocab_size = len(chars)
 char_to_ix = {ch: i for i, ch in enumerate(chars)}
 ix_to_char = {i: ch for i, ch in enumerate(chars)}
-I = numpy.eye(vocab_size)
 inputs = numpy.empty((nsamples, seq_length), dtype='uint8')
 outputs = numpy.zeros_like(inputs)
 for i, p in enumerate(range(0, len(data) - 1, seq_length)):
